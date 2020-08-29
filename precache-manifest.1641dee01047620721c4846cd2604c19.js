7e1a398ec6b39d8065e82517810b1ef0036c4756
Coolcon1729 <crawling@uwaterloo.ca>
2020-08-28 22:23:44 -0400

    Updates

---
+++ precache-manifest.1641dee01047620721c4846cd2604c19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee59fac1573e37b9c73472105482e60d",
+    "url": "/TicTacToe-Application/index.html"
+  },
+  {
+    "revision": "46bce3b49a46f5a90c13",
+    "url": "/TicTacToe-Application/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "a62a8f764bfff77b11dd",
+    "url": "/TicTacToe-Application/static/js/2.0ca3f1ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe-Application/static/js/2.0ca3f1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46bce3b49a46f5a90c13",
+    "url": "/TicTacToe-Application/static/js/main.4c0b23c0.chunk.js"
+  },
+  {
+    "revision": "a92da9d8b1eeeb2c5d39",
+    "url": "/TicTacToe-Application/static/js/runtime-main.a3983b9b.js"
+  }
+]);
